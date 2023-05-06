010eae98bbaf68bf2a662f7daa0686a2f6a36c9d
n.e.hullegien@student.tudelft.nl <n.e.hullegien@student.tudelft.nl>
2023-05-06 19:16:53 +0200

    Add regex to extract ytid

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,4 @@
-import { TextArea } from "@blueprintjs/core";
+import { TextArea, Text } from "@blueprintjs/core";
 import type { FormEvent} from "react";
 import { useState } from "react";
 
@@ -6,13 +6,28 @@ export default function Index() {
   const [youtubeId, setYtId] = useState<string>();
 
 
+  function matchYoutubeUrl(url: string){
+    var r, rx = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/|shorts\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
+    r = url.match(rx);
+    if (r == null) {
+      return "";
+    }
+    
+    return r[1];
+  }
+
+  
 
   return (
     <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
       <div>
         <h1>Hello hello!</h1>
-        <TextArea value={youtubeId} onChange={(textContent: FormEvent) => setYtId((textContent.target as HTMLInputElement).value)} />
-        <iframe src={`https://api.vevioz.com/api/widget/mp3/${youtubeId}`} style={{width:"100%", height:"150px", border:"none"}} title="IFrame" scrolling="no"></iframe>
+        <p>DEBUGGING: SEE YT ID BELOW AFTER UPLOAD</p>
+        <Text>{youtubeId}</Text>
+        <TextArea onChange={(textContent: FormEvent) => setYtId(matchYoutubeUrl((textContent.target as HTMLInputElement).value))} />
+        {youtubeId && 
+          <iframe src={`https://api.vevioz.com/api/widget/mp3/${youtubeId}`} style={{width:"100%", height:"150px", border:"none"}} title="IFrame" scrolling="no"></iframe>
+        }
       </div>
     </main>
   );
